be4c0fb4aa13e079d5aed91421c98cf5c6d4c051
Hamad alghanim <dreamersoul1@gmail.com>
2018-11-10 17:56:35 +0300

    bootstrap

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,79 @@
 <template>
   <div class="hello">
-    <h1>My List</h1>
-    <ol>
-      <todo-item
-        v-for="item in todos"
-        v-bind:todo="item"
-        v-bind:key="item.id">
-      </todo-item>
-    </ol>
+    <b-navbar toggleable type="light" variant="light">
+        <b-navbar-toggle target="nav_text_collapse"></b-navbar-toggle>
+        <b-navbar-brand>BootstrapVue</b-navbar-brand>
+        <b-collapse is-nav id="nav_text_collapse">
+            <b-navbar-nav>
+                <b-nav-text>Navbar text</b-nav-text>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+    <div class="container">
+      <b-alert show variant="primary">Primary Alert</b-alert>
+      <b-btn v-b-modal.modal1>Launch demo modal</b-btn>
+      <div class="my-3">
+        <b-btn v-b-popover.hover="'I am popover content!'" title="Popover Title">
+          Hover Me
+        </b-btn>
+      </div>
+      <b-table striped hover :items="items"></b-table>
+    </div>
+    <!-- Modal Component -->
+    <b-modal id="modal1" title="Bootstrap-Vue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
   </div>
 </template>
 
 <script>
-import TodoItem from './TodoItem.vue'
+  import Vue from 'vue'
+  import BootstrapVue from 'bootstrap-vue'
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-export default {
-  name: 'HelloWorld',
-  components: {
-    TodoItem
-  },
-  data: function(){
-    return {
-      todos:[
-        {id: 1, text: "todo No. 1"},
-        {id: 2, text: "todo No. 2"},
-        {id: 3, text: "todo No. 3"},
-      ]
-    }
-  },
-}
+  Vue.use(BootstrapVue);
+const items = [
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+]
+  export default {
+    name: 'HelloWorld',
+    components: {
+      BootstrapVue
+    },
+    data: function () {
+      return {
+        items: items
+      }
+    },
+  }
 </script>
 
 <style scoped>
-.hello {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  
-}
-input {
-      width: 30%;
-      padding: 12px 12px;
+  .hello {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  input {
+    width: 30%;
+    padding: 12px 12px;
     margin: 8px 0;
     box-sizing: border-box;
-}
-ol {
-  padding: 0px;
-  list-style-position: inside;
-}
-li {
-  margin: 0 10px;
-}
+  }
+
+  ol {
+    padding: 0px;
+    list-style-position: inside;
+  }
+
+  li {
+    margin: 0 10px;
+  }
 </style>
